7a4c7f3bf8190f16b95d7be419c2d503eb3b1dd4
mait <4n5XnA>
2015-03-25 21:39:50 +0200

    Initial commit

--- server.js
+++ server.js
@@ -6,10 +6,32 @@ var app = express();
 var timer_running = false;
 var time = 10;
 var status = 'game';
+var bodyParser = require('body-parser');
 var drupalUrl = 'fasndfask';
 var gameData =  {};
+var _ = require ('underscore');
 var usersInDraw = [];
+var registered = false;
 var file = fs.readFileSync("./game.json", "utf8");
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.post('/validate-user', function(req, res){
+  var user = req.body.email;
+  console.log(req.body.email);
+  res.send('4');
+  res.end('end');
+});
 
 app.post('/addgoal', function (req, res) {
   var user = req.body.id;
@@ -24,7 +46,6 @@ app.post('/addgoal', function (req, res) {
       gameData = response.data;
       wss.broadcast({type: 2, status:"game", data: gameData});
       updateData();
-
     }
   });
 });
@@ -35,10 +56,10 @@ app.get('/timer', function (req, res) {
   res.send('Get !');
 });
 
-app.get('/draw', function (req, res) {
-  status = 'draw';
-  wss.broadcast(JSON.stringify({ type: 1, status: 'draw'}));
-  res.send('draw')
+app.get('/start', function (req, res) {
+    console.log('Start Timer');
+
+    res.send('Timer!')
 });
 
 app.get('/players', function (req, res) {
@@ -57,8 +78,34 @@ app.get('/timer-end', function(res, req)  {
   wss.broadcast(JSON.stringify({ status: 'en'}))
 });
 
-app.get('api/add-user', function(res, req) {
+app.get('/add-user', function(res, req) {
+  var status = _.indexOf(usersInDraw, req.body.email);
 
+  if (status != -1) {
+    console.log('Kasutaja osaleb juba loosimises');
+    res.send(false);
+  } else {
+    usersInDraw.push(req.body.email);
+    res.send(true);
+  }
+  console.log('body: ' + req.body.email);
+});
+
+app.post('/participating', function(req, res) {
+  console.log(req.body);
+  var status = _.indexOf(usersInDraw, req.body.email);
+  if (status != -1) {
+    res.send(true);
+  } else {
+    res.send(false);
+  }
+  res.end('end');
+});
+
+app.post('/remove-user', function(req, res){
+  var index =_.indexOf(usersInDraw, req.body.email);
+  delete usersInDraw[index];
+  res.send(true);
 });
 
 app.get('/status', function(req, res) {
@@ -81,14 +128,16 @@ wss.broadcast = function broadcast(data) {
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     message = JSON.parse(message);
-    if(message.event == 'getStatus') {
+    if(message.event == 'getData') {
       wss.broadcast(JSON.stringify({ type: 1, status: status}));
-      //wss.broadcast(JSON.stringify({ type: 1, status: getStatus()}));
-      if(getStatus() == 'game') {
-        //todo check, if data exists
-        //wss.broadcast(JSON.stringify(gameData));
         wss.broadcast(JSON.stringify(getRelevantData()));
-      }
+
+     /*   getRelevantData2(function(err, data) {
+          if(data) {
+            wss.broadcast(JSON.stringify({ type: 1, status: 'game', data: data}));
+          }
+        });*/
+
     }
 
     if(message.event == 'userData') {
@@ -106,11 +155,13 @@ function timer() {
       if (timer_running <= 0) {
         timer_running = false;
         wss.broadcast(JSON.stringify({type: 3, status: "draw", time: "end"}));
-        setTimeout(function()  {
-          status = 'game';
-          wss.broadcast(JSON.stringify({type: 1, status: "game"}));
-          console.log("game")
-        },3000);
+        timerEnded(function(err, data) {
+          if(data) {
+            usersInDraw = [];
+            wss.broadcast(JSON.stringify({ type: 1, status: 'game', data: data}));
+
+          }
+        });
 
         clearInterval(i);
       }
@@ -123,40 +174,20 @@ function getStatus() {
   return 'game';
 }
 
-/*  praticipating(function (err, ret2) {
-    if (err) {
-      console.log('error');
-      view_error();
-      return;
+function getRelevantData2(callback) {
+  request({
+    uri: drupalUrl + '/get-data',
+    method: 'POST'
+  }, function(err, response) {
+    if (response) {
+      callback(false, response);
     }
-    if (ret2) {
-      view_participating();
-    } else {
-      view_timer();
+    if(err) {
+      console.error('Get-data error');
+      callback(true, err);
     }
-  });*/
-  /*function praticipating(callback) {
-    chrome.storage.local.get('email', function (data) {
-      var email = data.email;
-
-      var data = {
-        'email': email
-      };
-
-      jQuery.ajax({
-        type: 'POST',
-        url: nodeUrl + ':3000/participating',
-        data: data,
-        success: function (response) {
-          callback(false, response);
-        },
-        error: function () {
-          callback(true);
-        }
-      });
-    });
-  }*/
-
+  });
+}
 
 function getRelevantData() {
   var file2 = fs.readFileSync("./game.json", "utf8");
@@ -190,5 +221,21 @@ function updateData() {
 
 }
 
-
+function timerEnded(timerCallback) {
+  request({
+    uri: drupalUrl + '/api/get-players',
+    method: 'POST',
+    json: {
+      participants: usersInDraw
+    }
+  }, function(err, response) {
+    if (response) {
+      timerCallback(false, response);
+    }
+    if(err) {
+      console.error('Get-players error');
+      timerCallback(true, err);
+    }
+  });
+}
 //  app.get('/users', middleware1, middleware2, middleware3, processRequest);
